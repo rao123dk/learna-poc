9e260b609d87e00aefe60747264c16b269ebf290
rao123dk <dheerajkrao1993@gmail.com>
2019-01-30 18:59:15 +0530

    require thr.. scope chang

--- packages/newAvtar/lib/newAvtar.js
+++ packages/newAvtar/lib/newAvtar.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = newAvtar;
-const avtar = require('@rao/avatar')
-//import  avtar from '../../avatar/lib/avatar';
+//const avtar = require('@rao/avatar')
+import  avtar from '@rao/avatar';
 
 
 function newAvtar() {
